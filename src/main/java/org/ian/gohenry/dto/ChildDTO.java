fbe24c99d227cea70eefe3bfeceab9019ac8bff9
ian <ian@example.com>
2018-08-11 17:06:19 +0100

    added in validation rules

--- src/main/java/org/ian/gohenry/dto/ChildDTO.java
+++ src/main/java/org/ian/gohenry/dto/ChildDTO.java
@@ -4,29 +4,43 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
-@JsonPropertyOrder({"id","title","firstName","lastName", "emailAddress", "dateOfBirth","gender", "secondName"})
+
+@JsonPropertyOrder({"id", "title", "firstName", "lastName", "emailAddress", "dateOfBirth", "gender", "secondName"})
 public class ChildDTO {
 
     private Long id;
+    @NotNull(message = "must not be null")
+    @NotEmpty(message = "must not be empty")
     private String firstname;
     private String secondName;
+    @NotNull(message = "must not be null")
+    @NotEmpty(message = "must not be empty")
     private String lastname;
+    @Email(message = "must be an email address")
     private String emailaddress;
+    @Past(message = "date of birth must be in the past")
     private LocalDate dateofbirth;
+    @NotNull(message = "must not be null")
+    @Pattern(message = "must be male or female", regexp = "^(male|female)$")
     private String gender;
 
     public ChildDTO() {
     }
 
     @JsonCreator
-    public ChildDTO(@JsonProperty("id")Long id,
-                 @JsonProperty("firstName")String firstname,
-                 @JsonProperty("lastName")String lastname,
-                 @JsonProperty("secondName")String secondname,
-                 @JsonProperty("emailAddress")String emailaddress,
-                 @JsonProperty("dateOfBirth") LocalDate dateofbirth,
-                 @JsonProperty("gender")String gender) {
+    public ChildDTO(@JsonProperty("id") Long id,
+                    @JsonProperty("firstName") String firstname,
+                    @JsonProperty("lastName") String lastname,
+                    @JsonProperty("secondName") String secondname,
+                    @JsonProperty("emailAddress") String emailaddress,
+                    @JsonProperty("dateOfBirth") LocalDate dateofbirth,
+                    @JsonProperty("gender") String gender) {
         this.id = id;
         this.firstname = firstname;
         this.secondName = secondname;
@@ -35,35 +49,41 @@ public class ChildDTO {
         this.dateofbirth = dateofbirth;
         this.gender = gender;
     }
+
     @JsonProperty("id")
     public Long getId() {
         return id;
     }
+
     @JsonProperty("firstName")
     public String getFirstname() {
         return firstname;
     }
+
     @JsonProperty("secondName")
     public String getSecondName() {
         return secondName;
     }
+
     @JsonProperty("lastName")
     public String getLastname() {
         return lastname;
     }
+
     @JsonProperty("emailAddress")
     public String getEmailaddress() {
         return emailaddress;
     }
+
     @JsonProperty("dateofBirth")
     public LocalDate getDateofbirth() {
         return dateofbirth;
     }
+
     @JsonProperty("gender")
     public String getGender() {
         return gender;
     }
-
 
 
     public void setId(Long id) {
--- src/main/java/org/ian/gohenry/dto/ParentDTO.java
+++ src/main/java/org/ian/gohenry/dto/ParentDTO.java
@@ -3,20 +3,33 @@ package org.ian.gohenry.dto;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import org.ian.gohenry.domain.Child;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.util.Set;
 
-@JsonPropertyOrder({"id","title","firstName","lastName", "emailAddress", "dateOfBirth","gender", "secondName", "children"})
+@JsonPropertyOrder({"id", "title", "firstName", "lastName", "emailAddress", "dateOfBirth", "gender", "secondName", "children"})
 public class ParentDTO {
     private Long id;
+    @NotNull(message = "must not be null")
+    @NotEmpty(message = "must not be empty")
     private String title;
+    @NotNull(message = "must not be null")
+    @NotEmpty(message = "must not be empty")
     private String firstname;
+    @NotNull(message = "must not be null")
+    @NotEmpty(message = "must not be empty")
     private String lastname;
     private String secondname;
+    @Email(message = "must be an email address")
     private String emailaddress;
+    @Email(message = "must be an email address")
     private LocalDate dateofbirth;
+    @NotNull(message = "must not be null")
+    @Pattern(message = "must be male or female", regexp = "^(male|female)$")
     private String gender;
     private Set<ChildDTO> children;
 
@@ -24,15 +37,15 @@ public class ParentDTO {
     }
 
     @JsonCreator
-    public ParentDTO(@JsonProperty("id")Long id,
-                  @JsonProperty("title") String title,
-                  @JsonProperty("firstName")String firstname,
-                  @JsonProperty("secondName")String secondname,
-                  @JsonProperty("lastName")String lastname,
-                  @JsonProperty("emailAddress")String emailaddress,
-                  @JsonProperty("dateOfBirth")LocalDate dateofbirth,
-                  @JsonProperty("gender")String gender,
-                  @JsonProperty("children")Set<ChildDTO> children) {
+    public ParentDTO(@JsonProperty("id") Long id,
+                     @JsonProperty("title") String title,
+                     @JsonProperty("firstName") String firstname,
+                     @JsonProperty("secondName") String secondname,
+                     @JsonProperty("lastName") String lastname,
+                     @JsonProperty("emailAddress") String emailaddress,
+                     @JsonProperty("dateOfBirth") LocalDate dateofbirth,
+                     @JsonProperty("gender") String gender,
+                     @JsonProperty("children") Set<ChildDTO> children) {
         this.id = id;
         this.title = title;
         this.firstname = firstname;
@@ -43,38 +56,47 @@ public class ParentDTO {
         this.gender = gender;
         this.children = children;
     }
+
     @JsonProperty("id")
     public Long getId() {
         return id;
     }
+
     @JsonProperty("title")
     public String getTitle() {
         return title;
     }
+
     @JsonProperty("firstName")
     public String getFirstname() {
         return firstname;
     }
+
     @JsonProperty("lastName")
     public String getLastname() {
         return lastname;
     }
+
     @JsonProperty("secondName")
     public String getSecondname() {
         return secondname;
     }
+
     @JsonProperty("emailAddress")
     public String getEmailaddress() {
         return emailaddress;
     }
+
     @JsonProperty("dateOfBirth")
     public LocalDate getDateofbirth() {
         return dateofbirth;
     }
+
     @JsonProperty("gender")
     public String getGender() {
         return gender;
     }
+
     @JsonProperty("children")
     public Set<ChildDTO> getChildren() {
         return children;
