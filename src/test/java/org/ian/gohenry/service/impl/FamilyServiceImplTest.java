43cda87f81e6bde64db9c2c748c96a71bb80d098
ian <ian@example.com>
2018-08-11 19:48:55 +0100

    added in create test

--- src/test/java/org/ian/gohenry/service/impl/FamilyServiceImplTest.java
+++ src/test/java/org/ian/gohenry/service/impl/FamilyServiceImplTest.java
@@ -1,17 +1,42 @@
 package org.ian.gohenry.service.impl;
 
+import org.ian.gohenry.dal.ParentDAO;
+import org.ian.gohenry.domain.Parent;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FamilyServiceImplTest {
+    @Mock
+    private ParentDAO parentDAO;
+    @InjectMocks
+    private FamilyServiceImpl underTest;
 
     @Test
     public void shouldCreateParent() throws Exception {
+        Parent parent = new Parent();
+        parent.setId(1l);
+        parent.setEmailaddress("meh@meh.com");
 
+        when(parentDAO.save(parent)).thenReturn(parent);
+
+
+        Parent ret = underTest.createParent(parent);
+
+        assertThat(ret.getId(), is(1l));
+        assertThat(ret.getEmailaddress(), is("meh@meh.com"));
+
+        verify(parentDAO).save(parent);
     }
 
     @Test
