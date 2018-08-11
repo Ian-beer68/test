01af3c4413cf2cc318146b120f8242c35ebcb0b3
ian <ian@example.com>
2018-08-11 19:37:23 +0100

    test for family service

---
+++ src/test/java/org/ian/gohenry/service/impl/FamilyServiceImplTest.java
@@ -0,0 +1,22 @@
+package org.ian.gohenry.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FamilyServiceImplTest {
+
+    @Test
+    public void shouldCreateParent() throws Exception {
+
+    }
+
+    @Test
+    public void shouldGetParent() throws Exception {
+
+    }
+
+}
