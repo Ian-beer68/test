0d14763d92e80852d70ecec99881c933555cf8b8
ian <ian@example.com>
2018-08-11 16:40:51 +0100

    added in Repositories

--- src/main/java/org/ian/gohenry/dal/ChildDAO.java
+++ src/main/java/org/ian/gohenry/dal/ChildDAO.java
@@ -1,4 +1,10 @@
 package org.ian.gohenry.dal;
 
-public class ChildDAO {
+import org.ian.gohenry.domain.Child;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChildDAO extends JpaRepository<Child, Long> {
+
 }
--- src/main/java/org/ian/gohenry/dal/ParentDAO.java
+++ src/main/java/org/ian/gohenry/dal/ParentDAO.java
@@ -1,4 +1,10 @@
 package org.ian.gohenry.dal;
 
-public interface ParentDAO {
+import org.ian.gohenry.domain.Parent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParentDAO extends JpaRepository<Parent, Long> {
+
 }
